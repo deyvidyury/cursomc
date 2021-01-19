85d6f03bf7e4cf63ae1aaabec789a3870dbe640f
Deyvid Lopes <deyvidyury@yahoo.com.br>
2021-01-19 11:27:04 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/yury/cursomc/services/CategoriaService.java
+++ src/main/java/com/yury/cursomc/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.yury.cursomc.domain.Categoria;
 import com.yury.cursomc.repositories.CategoriaRepository;
+import com.yury.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objecto nao encontrado id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
