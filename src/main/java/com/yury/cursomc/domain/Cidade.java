3a90c8c03ed6cc639c2779803723a4d8a90ad4e2
Deyvid Lopes <deyvidyury@yahoo.com.br>
2021-01-20 10:17:14 -0300

    Cliente, TipoCliente, Telefone e enderecos

--- src/main/java/com/yury/cursomc/domain/Cidade.java
+++ src/main/java/com/yury/cursomc/domain/Cidade.java
@@ -2,12 +2,14 @@ package com.yury.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class Cidade implements Serializable {
 
 	/**
